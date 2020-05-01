18fd25bb49d221ffae3c4ebc1e090b3bd8c9ddef
LCSmith87 <LCSmith87@gmail.com>
2020-04-30 22:28:42 -0500

    added loading bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux'
+import { handleInitialData } from '../../actions/shared'
+import LoadingBar from 'react-redux-loading'
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+class App extends Component {
+    componentDidMount() {
+        this.props.dispatch(handleInitialData())
+        console.log(this.props.loading)
+    }
+    render() {
+        return (
+            <React.Fragment>
+            <div className="App">
+                <LoadingBar style={{ backgroundColor: '#ff267c'}} />
+            </div>
+            </React.Fragment>
+          )
+    }
 }
 
-export default App;
+function mapStateToProps ({ authedUser }) {
+    return {
+        loading: authedUser === null
+    }
+}
+
+export default connect(mapStateToProps)(App);
