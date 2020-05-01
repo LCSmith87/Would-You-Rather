f4119cfbf4c10dea5d4a89ca622aff8b51555d06
LCSmith87 <LCSmith87@gmail.com>
2020-05-01 17:54:04 -0500

    Set up new question page

--- src/components/NewQuestion/NewQuestion.js
+++ src/components/NewQuestion/NewQuestion.js
@@ -2,10 +2,50 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class NewQuestion extends Component {
+    state = {
+        optionOne: '',
+        optionTwo: '',
+    }
+    handleoptionOne = (e) => {
+        this.setState({
+            optionOne: e.target.value
+        })
+    }
+    handleoptionTwo = (e) => {
+        this.setState({
+            optionTwo: e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+        this.setState({
+            optionOne: '',
+            optionTwo: '',
+        })
+    }
     render() {
         return (
             <div>
-                New Question
+                <h2>Create New Question</h2>
+                <h3>Complete the question:</h3>
+                <h4>Would you rather ...</h4>
+                <form onSubmit={(e) => this.handleSubmit(e)}>
+                    <input
+                        type="text"
+                        value={this.state.optionOne}
+                        placeholder="Enter option one text here..."
+                        onChange={(e) => this.handleoptionOne(e)}
+                    />
+                    <hr /><span>OR</span><hr />
+                    <input
+                        type="text"
+                        value={this.state.optionTwo}
+                        placeholder="Enter option two text here..."
+                        onChange={(e) => this.handleoptionTwo(e)}
+                    />
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
