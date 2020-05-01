1c3a03c9b3c4428b1b5249e303b83a9725dabef8
LCSmith87 <LCSmith87@gmail.com>
2020-04-30 23:14:30 -0500

    Added home, login, and logout components - set up private routes for redirect to login page if not authed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,27 +3,60 @@ import './App.css';
 import { connect } from 'react-redux'
 import { handleInitialData } from '../../actions/shared'
 import LoadingBar from 'react-redux-loading'
+import Login from '../Login/Login'
+import Logout from '../Logout/Logout'
+import Home from '../Home/Home'
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect } from 'react-router-dom'
+
 
 
 class App extends Component {
     componentDidMount() {
         this.props.dispatch(handleInitialData())
-        console.log(this.props.loading)
     }
     render() {
+        const { authedUser } = this.props;
         return (
-            <React.Fragment>
-            <div className="App">
-                <LoadingBar style={{ backgroundColor: '#ff267c'}} />
-            </div>
-            </React.Fragment>
+            <Router>
+                <React.Fragment>
+                    <LoadingBar style={{ backgroundColor: '#ff267c'}} />
+                    <div className="App">
+                        <Switch>
+                            <Route path='/login' component={Login} />
+                            <PrivateRoute user={authedUser} >
+                                <Route exact path='/' component={Home} />
+                                <Route path='/logout' component={Logout} />
+                            </PrivateRoute>
+                        </Switch>
+                    </div>
+                </React.Fragment>
+            </Router>
           )
     }
 }
 
+function PrivateRoute({ children, user, ...rest}) {
+    return (
+        <Route
+            {...rest}
+            render={({ location }) =>
+                user !== null ? (
+                    children
+                ) : (
+                    <Redirect to='/login' />
+                )
+            }
+        />
+    )
+}
+
 function mapStateToProps ({ authedUser }) {
     return {
-        loading: authedUser === null
+        authedUser
     }
 }
 
