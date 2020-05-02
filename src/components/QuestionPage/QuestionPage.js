cf1f23381983f2413fccb55ecd1dfca060d9b4f9
LCSmith87 <LCSmith87@gmail.com>
2020-05-02 00:33:43 -0500

    removed unused import

--- src/components/QuestionPage/QuestionPage.js
+++ src/components/QuestionPage/QuestionPage.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { handleQuestions } from '../../actions/shared'
 
 class QuestionPage extends Component {
     render() {
