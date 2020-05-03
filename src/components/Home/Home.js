d0f1d5b198d9920258c0298ecb0fa0bda6e85bf6
LCSmith87 <LCSmith87@gmail.com>
2020-05-03 00:18:25 -0500

    Sorted questions by timestamp

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,10 +4,10 @@ import QuestionCard from '../QuestionCard/QuestionCard'
 
 class Home extends Component {
     render() {
-        const { questions, users } = this.props
+        const { questions, questionsSorted, users } = this.props
         return (
             <div>
-                {Object.keys(questions).map((question) => {
+                {questionsSorted.map((question) => {
                     const user = users[questions[question].author];
                     return (
                         <QuestionCard author={user} key={questions[question].id} question={questions[question]} />
@@ -22,6 +22,7 @@ function mapStateToProps ({ authedUser, questions, users }) {
     return {
         authedUser,
         questions,
+        questionsSorted: Object.keys(questions).sort((a,b) => questions[b].timestamp - questions[a].timestamp),
         users
     }
 }
