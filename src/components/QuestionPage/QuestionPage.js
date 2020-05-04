7a3784958eaf2ae9bc01217757bf1508d1425d56
LCSmith87 <LCSmith87@gmail.com>
2020-05-03 23:37:10 -0500

    added form to poll page

--- src/components/QuestionPage/QuestionPage.js
+++ src/components/QuestionPage/QuestionPage.js
@@ -3,9 +3,31 @@ import { connect } from 'react-redux'
 import { Redirect } from 'react-router'
 
 class QuestionPage extends Component {
+    state = {
+        selectedOption: 'optionOne'
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        // todo: save answer
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            selectedOption: e.target.value
+        }, () => {
+            console.log(this.state.selectedOption)
+        })
+    }
     render() {
+
+        // Check for a valid question and redirect if not found
         if(!this.props.questions) {
            return <Redirect to="/404" />
+        }
+        const id = this.props.id
+        if(this.props.answeredQuestions.includes(id)) {
+            return <Redirect to={`/question/${id}/result`} />
         }
         const users = this.props.users
         const author = users[this.props.questions.author].name
@@ -15,11 +37,37 @@ class QuestionPage extends Component {
         return (
             <div>
                 <div className="question">
-                    <img src={avatar} alt={`${author}'s avatar`} />
-                    <h2>{author} asks:</h2>
-                    <h3>Would You Rather?</h3>
-                    <p>{optionOneText} or... {optionTwoText}</p>
-                    <button>Submit Answer</button>
+                    <div className="question-header">
+                        <h2>{author} asks:</h2>
+                    </div>
+                    <div className="question-card">
+                        <div className="question-card-left">
+                            <img src={avatar} alt={`${author}'s avatar`} />
+                        </div>
+                    <div className="question-card-right">
+                        <form>
+                            <h3>Would You Rather?</h3>
+                            <input
+                                type="radio"
+                                name="options"
+                                id="optionOne"
+                                value="optionOne"
+                                onClick={(e) => this.handleChange(e)}
+                            />
+                            <label htmlFor="optionOne">{optionOneText}</label>
+                            <input
+                                type="radio"
+                                name="options"
+                                id="optionTwo"
+                                value="optionTwo"
+                                onClick={(e) => this.handleChange(e)}
+                            />
+                            <label htmlFor="optionTwo">{optionTwoText}</label>
+
+                            <button>Submit Answer</button>
+                        </form>
+                    </div>
+                    </div>
                 </div>
             </div>
         )
@@ -33,6 +81,7 @@ function mapPropsToState({ authedUser, users, questions}, props) {
         id,
         users,
         authedUser,
+        answeredQuestions: Object.keys(authedUser["answers"]),
         questions: !questions[id]
             ? null
             : questions[id]
